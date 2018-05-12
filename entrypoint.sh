6fc50352a7a6c0f81586edccf70e033aa6c3dc8a
Wyntau <wyntau@outlook.com>
2018-05-12 18:42:01 +0800

    delay 10s

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 if [ ! -e "/app/runtime/init.lock" ]; then
-  echo "========================"
-  echo "Installing server ......"
-  echo "========================"
-  sleep 6s
+  echo "============================="
+  echo "Install server after 10s....."
+  echo "============================="
+
+  sleep 10s
+
+  echo "============================="
+  echo "Installing server ..........."
+  echo "============================="
   npm run install-server
 fi;
 
