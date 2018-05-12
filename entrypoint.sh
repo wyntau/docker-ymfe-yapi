d6fa4b107a37f7575de57c3727e4cbd14c68b486
Wyntau <wyntau@outlook.com>
2018-05-12 17:42:42 +0800

    refactor: 延迟初始化数据库

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ if [ ! -e "/app/runtime/init.lock" ]; then
   echo "========================"
   echo "Installing server ......"
   echo "========================"
+  sleep 6s
   npm run install-server
 fi;
 
