339b61aa7f9d7862774cb3afe9b8d509c95020ff
Wyntau <wyntau@outlook.com>
2018-05-18 14:44:53 +0800

    refactor: sleep before install server or connect to mongodb

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,18 @@
 
 if [ ! -e "/app/runtime/init.lock" ]; then
   echo "============================="
-  echo "Install server after 10s....."
+  echo "Install server after 6s....."
   echo "============================="
-
-  sleep 10s
-
+  sleep 6s
   echo "============================="
   echo "Installing server ..........."
   echo "============================="
   npm run install-server
+else
+  echo "============================="
+  echo "Connect MongoDB after 3s ...."
+  echo "============================="
+  sleep 3s
 fi;
 
 node /app/vendors/server/app.js
